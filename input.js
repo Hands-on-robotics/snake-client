ca40ce3c2702816def9a21477bb1e16089d0ea39
Evan Hands <handsonevanoff@gmail.com>
2023-09-18 07:14:19 -0700

    fixed console log bug

--- input.js
+++ input.js
@@ -5,28 +5,24 @@ let connection;
 
 
 // callback for setupInput()
-const handleUserInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', (key) => {
-    
-    // to exit "Ctrl + C"
-    if (key === '\u0003') {
-      process.stdout.write('Ctrl+C pressed. Now exiting...');
-      process.exit();
-    }  else if (key === "w") {
-      connection.write("Move: up");
-      console.log("Pressed: Up");
-    } else if (key === "a") {
-      connection.write("Move: left");
-      console.log("Pressed: Left");
-    } else if (key === "s") {
-      connection.write("Move: down");
-      console.log("Pressed: Down");
-    } else if (key === "d") {
-      connection.write("Move: right");
-      console.log("Pressed: Right");
-    }
-  });
+const handleUserInput = function(key) {
+  // to exit "Ctrl + C"
+  if (key === '\u0003') {
+    process.stdout.write('Ctrl+C pressed. Now exiting...');
+    process.exit();
+  }  else if (key === "w") {
+    connection.write("Move: up");
+    console.log("Pressed: Up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+    console.log("Pressed: Left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+    console.log("Pressed: Down");
+  } else if (key === "d") {
+    connection.write("Move: right");
+    console.log("Pressed: Right");
+  }
   
 };
 
